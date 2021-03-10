f908cfa7cc9987e44ca959b46ed596eaa1a7b7c4
Hunter Peavey <srht@krathalan.net>
2021-03-09 20:39:59 -0800

    Update linux-tkg postpull script

--- linux-tkg/krack-postpull.sh
+++ linux-tkg/krack-postpull.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+cp /home/builder/git/build-alterations/linux-tkg/customization.cfg /home/builder/.local/share/krack/packages/linux-tkg/customization.cfg
 patch linux-tkg-config/prepare prepare-noconfirm.patch
