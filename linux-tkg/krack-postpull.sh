55ea4c7fbd71f37391735b78e04ae68e5592e409
Hunter Peavey <srht@krathalan.net>
2021-03-13 00:54:56 -0800

    Rename files

--- linux-tkg/krack-postpull.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cp /home/builder/git/build-alterations/linux-tkg/customization.cfg /home/builder/.local/share/krack/packages/linux-tkg/customization.cfg
-patch linux-tkg-config/prepare prepare-noconfirm.patch
