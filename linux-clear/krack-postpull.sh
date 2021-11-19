18cd3b2f1ef147f78ac77d6f3aebcb78076aee47
Hunter Peavey <dev@krathalan.net>
2021-11-18 17:46:51 -0800

    Add linux-clear alterations

---
+++ linux-clear/krack-postpull.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+printf "\
+diff --git a/PKGBUILD b/PKGBUILD
+     index a090765..5bd1d6d 100644
+     --- a/PKGBUILD
+     +++ b/PKGBUILD
+     @@ -62,7 +62,7 @@ _localmodcfg=
+      #  39. Generic-x86-64-v4 (GENERIC_CPU4)
+      #  40. Intel-Native optimizations autodetected by GCC (MNATIVE_INTEL)
+      #  41. AMD-Native optimizations autodetected by GCC (MNATIVE_AMD)
+     -_subarch=
+     +_subarch=38
+      
+      # Use the current kernel's .config file
+      # Enabling this option will use the .config of the RUNNING kernel rather than
+" > /tmp/krack-post.patch
+
+patch PKGBUILD /tmp/krack-post.patch
+rm -f /tmp/krack-post.patch
