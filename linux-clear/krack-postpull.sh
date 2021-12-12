91f1c3987703c413b1ca8c353bf94bdc653543f7
Hunter Peavey <dev@krathalan.net>
2021-12-12 11:26:25 -0800

    Update linux-clear patch

--- linux-clear/krack-postpull.sh
+++ linux-clear/krack-postpull.sh
@@ -2,18 +2,26 @@
 
 printf "\
 diff --git a/PKGBUILD b/PKGBUILD
-     index a090765..5bd1d6d 100644
-     --- a/PKGBUILD
-     +++ b/PKGBUILD
-     @@ -62,7 +62,7 @@ _localmodcfg=
-      #  39. Generic-x86-64-v4 (GENERIC_CPU4)
-      #  40. Intel-Native optimizations autodetected by GCC (MNATIVE_INTEL)
-      #  41. AMD-Native optimizations autodetected by GCC (MNATIVE_AMD)
-     -_subarch=
-     +_subarch=38
-      
-      # Use the current kernel's .config file
-      # Enabling this option will use the .config of the RUNNING kernel rather than
+index bb5b71d..e0035c8 100644
+--- a/PKGBUILD
++++ b/PKGBUILD
+@@ -62,7 +62,7 @@
+ #  39. Generic-x86-64-v4 (GENERIC_CPU4)
+ #  40. Intel-Native optimizations autodetected by GCC (MNATIVE_INTEL)
+ #  41. AMD-Native optimizations autodetected by GCC (MNATIVE_AMD)
+-: \"\${_subarch:=\"\"}\"
++: \"\${_subarch:=\"38\"}\"
+
+ # Use the current kernel's .config file
+ # Enabling this option will use the .config of the RUNNING kernel rather than
+@@ -72,7 +72,7 @@
+ : \"\${_use_current:=\"\"}\"
+
+ # Enable compiling with LLVM
+-: \"\${_use_llvm_lto:=\"\"}\"
++: \"\${_use_llvm_lto:=\"Y\"}\"
+
+ ### IMPORTANT: Do no edit below this line unless you know what you're doing
 " > /tmp/krack-post.patch
 
 patch PKGBUILD /tmp/krack-post.patch
